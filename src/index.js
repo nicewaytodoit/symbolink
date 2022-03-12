bf0448fa5bdb4c176f93e3d5b9ed72b6f2c6fe4d
Aleksandar Ristevski <nice@ristevski.me>
2022-03-12 11:33:22 +0000

    Adding functionality for Symbolink folder linking

--- src/index.js
+++ src/index.js
@@ -3,41 +3,116 @@
 const commandLineArgs = require('command-line-args');
 const symlinkDir = require('symlink-dir');
 const path = require('path');
+const fs = require('fs');
+
+const COLOR = {
+    end: '\x1b[0m',
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+    crimson: '\x1b[38m',
+};
+
+const log = console.log;
+const loge = (msg) => log(COLOR.red, msg, COLOR.end);
+
+const isEmpty = (value) => ['null', 'undefined', ''].includes((new String(value)).toString().trim());
 
 let options = undefined;
 try {
     options = commandLineArgs([
-        {
-            name: 'file',
-            alias: 'f',
-            type: String,
-            multiple: false,
-        },
+        { name: 'file', alias: 'f',  type: String, multiple: false },
+        { name: 'root', alias: 'r',  type: String, multiple: false },
+        { name: 'link', alias: 'l',  type: String, multiple: false },
+        { name: 'dest', alias: 'd',  type: String, multiple: false },
     ]);
 } catch (error) {
     if (error?.name === 'UNKNOWN_OPTION') {
-        console.log(`Unknown Option: [${error?.optionName}]`);
+        log(`Unknown Option: [${error?.optionName}]`);
     }
 }
 
-// grab provided args.
-const [, , ...args] = process.argv;
+const currentRoot = process.cwd();
+// const [, , ...args] = process.argv;
+// log(`Hello World [${args}]`);
 
-// Printe helo world provided args.
-console.log(`Hello World [${args}]`);
+// log(options);
+// log(__dirname);
+// log(process.cwd()); 
 
-console.log(options);
+const getPath = (file, root = currentRoot) => path.isAbsolute(file) ? file : path.join(root, file);
 
-// Any file or directory, that has the destination name, is renamed before creating the link
-// symlinkDir('b', 'c/b/')
-//   .then(result => {
-//     console.log('Message:', result);
-//     //> { reused: false }
+const createAllSymLinks = (links) => {
+    // Any file or directory, that has the destination name, is renamed before creating the link
+    Promise
+        .all(links.map((lnk) => new Promise((resolve, reject) => {
+            symlinkDir(lnk.dest, lnk.link)
+                .then((result) => {
+                    return resolve({...result, link: lnk.link});
+                })
+                .catch((err) => {
+                    return reject(err)
+                });
+        })))
+        .then((results) => {
+            for(var res of results) {
+                if (res?.warn) log(COLOR.magenta, `Warning: [${res?.warn}]`, COLOR.end);
+            }
+            log(COLOR.green, 'Link creation done !', COLOR.end);
+        })
+        .catch((err) => {
+            loge(err)
+        });
+}
 
-//     // return symlinkDir('b', 'c/a');
-//     // return "This is not ok";
-//   })
-//   .catch(err => console.error(err))
+const getLinks = (links, root) => links
+    .filter((lnk) => !isEmpty(lnk?.link) && !isEmpty(lnk?.dest))
+    .map((lnk) => ({ link: getPath(lnk?.link, root), dest: getPath(lnk?.dest, root) }));
 
+
+if (options?.file) {
+    const filePath = getPath(options?.file);
+    if (!fs.existsSync(filePath)) {
+        loge('Config file does not exist', filePath)
+    } else {
+        const configFile = fs.readFileSync(filePath);
+
+        let config = {};
+        try {
+            config = JSON.parse(configFile);
+        } catch (error) {
+            loge(`ERROR: JSON Parse issue: ${error?.message}`);
+            return;
+        }
+
+        const root = isEmpty(config?.root) ? currentRoot : getPath(config?.root);
+        
+        if (!Array.isArray(config?.links)) { 
+            loge('ERROR: Sym links configuration missing'); 
+        } else {
+            const links = getLinks(config?.links, root);
+            for(var a of links) {
+                log(COLOR.yellow, `[@](${a.link})`, COLOR.blue, `===> ${a.dest}`, COLOR.end);
+            }
+            if (links.length > 0) createAllSymLinks(links);
+        }
+    }
+} else {
+    const root = isEmpty(options?.root) ? currentRoot : getPath(options?.root);
+    const links = getLinks([{ link: options?.link, dest: options?.dest }], root);
+
+    if (links.length > 0) {
+        createAllSymLinks(links);
+    } else {
+        loge('Config file not present or link options not provided');
+    }
+}
+
+// [i]dea : Going to version and doing everything with symlinks from one point package@2.3.1 symlink from nodemodules 
 // chmod +x cli.js
 // npm unlink symbolink
---
+++ src/test.js
@@ -0,0 +1,36 @@
+var Promise = require("bluebird");
+
+Promise.config({ cancellation: true });
+
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The first promise has resolved');
+        resolve(10);
+    }, 1 * 1000);
+}).then(a => { console.log('then1'); return a; });;
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log('The second promise has rejected');
+        reject('Failed');
+    }, 2 * 1000);
+}).then(a => { console.log('then2'); return a; });;
+
+const p3 = new Promise((resolve, reject, onCancel) => {
+    var timer =  setTimeout(() => {
+        console.log('The third promise has resolved');
+        resolve(30);
+    }, 3 * 1000);
+    onCancel(_ => {
+        clearTimeout(timer);
+        console.log('cancelled 3');
+    });
+}).then(a => { console.log('then3'); return a; });;
+
+var promises = [p1, p2, p3];
+Promise.all(promises)
+    .then(console.log) // never execute
+    .catch((err) => {
+        console.log(err);
+        promises.forEach(p => p.cancel());
+    });
